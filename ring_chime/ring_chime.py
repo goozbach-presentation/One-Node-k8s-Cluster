2b269f27a8ce43a6458e5d36f6a8ae947c198413
Derek Carter <derek@goozbach.com>
2017-07-12 00:41:23 -0600

    ring_chime done

--- ring_chime/ring_chime.py
+++ ring_chime/ring_chime.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+mydoors = ['front', 'back', 'side']
 
-def index():
-    return "hello, world"
+@app.route('/api/v0.1/ring_chime/<string:door>', methods=["Get"])
+
+def ring_chime(door):
+    print door
+    if door not in mydoors:
+        retobj = { 'status': 'fail', 'msg': 'door does not exist', 'door': door}
+    else:
+        retobj = { 'status': 'success', 'ringing': door }
+    return jsonify(retobj)
 
 if __name__ == '__main__':
     app.run(debug=True)
